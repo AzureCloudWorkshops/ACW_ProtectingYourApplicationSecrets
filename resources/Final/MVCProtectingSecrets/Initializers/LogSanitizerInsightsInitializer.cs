fce967193cecf040ae7201f4f75c67fe16b7bbaf
Brian L. Gorman <blgorman@gmail.com>
2024-04-18 14:59:33 -0500

    updates for .NET 8 and new Azure functionality completed

--- resources/Final/MVCProtectingSecrets/Initializers/LogSanitizerInsightsInitializer.cs
+++ resources/Final/MVCProtectingSecrets/Initializers/LogSanitizerInsightsInitializer.cs
@@ -3,69 +3,67 @@ using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using System.Text.RegularExpressions;
 
-namespace MVCProtectingSecrets.Initializers
+namespace MVCProtectingSecrets.Initializers;
+
+public class LogSanitizerInsightsInitializer : ITelemetryInitializer
 {
-    public class LogSanitizerInsightsInitializer : ITelemetryInitializer
+    public void Initialize(ITelemetry telemetry)
     {
-        public void Initialize(ITelemetry telemetry)
+        var traceTelemetry = telemetry as TraceTelemetry;
+
+        if (traceTelemetry != null)
         {
-            var traceTelemetry = telemetry as TraceTelemetry;
-
-            if (traceTelemetry != null)
-            {
-                traceTelemetry.Message = SanitizeString(traceTelemetry.Message);
-                // If we don't remove this CustomDimension, the telemetry message will still contain the PII in the "OriginalFormat" property.
-                traceTelemetry.Properties.Remove("OriginalFormat");
-            }
+            traceTelemetry.Message = SanitizeString(traceTelemetry.Message);
+            // If we don't remove this CustomDimension, the telemetry message will still contain the PII in the "OriginalFormat" property.
+            traceTelemetry.Properties.Remove("OriginalFormat");
         }
+    }
 
-        public static string SanitizeString(string msg)
-        {
-            // Sanitize email addresses
-            msg = SanitizeEmail(msg);
+    public static string SanitizeString(string msg)
+    {
+        // Sanitize email addresses
+        msg = SanitizeEmail(msg);
+        // Sanitize connection strings
+        msg = SanitizeConnectionStringDetails(msg);
+        // Sanitize SAS Tokens
+        msg = SanitizeSASToken(msg);
+        // Add more sanitization logic here as needed
 
-            // Sanitize connection strings
-            msg = SanitizeConnectionStringDetails(msg);
+        //return sanitized string
+        return msg;
+    }
 
-            // Sanitize SAS Token
-            msg = SanitizeSASToken(msg);
+    private static string SanitizeEmail(string msg)
+    {
+        var regexEmail = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+        var replacedEmail = "[emailaddress]";
+        return Regex.Replace(msg, regexEmail, replacedEmail);
+    }
 
-            //return sanitized string
-            return msg;
-        }
+    private static string SanitizeConnectionStringDetails(string msg)
+    {
+        var regexSQLConnectionString = @"(?i)[a-z][a-z0-9-]+\.database(?:\.secure)?\.(?:(?:windows|usgovcloudapi)\.net|chinacloudapi\.cn|cloudapi\.de)";
+        var regexInitialCatalog = @"Initial Catalog=[a-zA-Z0-9-]*";
+        var regexUserID = @"User ID=[a-zA-Z0-9-_]*";
+        var regexPassword = @"Password=[a-zA-Z0-9-!@#$%^&*()_]*";
 
-        private static string SanitizeEmail(string msg)
-        {
-            var regexEmail = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
-            var replacedEmail = "[emailaddress]";
-            return Regex.Replace(msg, regexEmail, replacedEmail);
-        }
+        var cnstrReplaced = "[redacted-server]";
+        var catReplace = "Intiial Catalog=[redacted-db]";
+        var userIDReplace = "User ID=[redacted-user]";
+        var passwordReplace = "Password=[redacted-password]";
 
-        private static string SanitizeConnectionStringDetails(string msg)
-        {
-            var regexSQLConnectionString = @"(?i)[a-z][a-z0-9-]+\.database(?:\.secure)?\.(?:(?:windows|usgovcloudapi)\.net|chinacloudapi\.cn|cloudapi\.de)";
-            var regexInitialCatalog = @"Initial Catalog=[a-zA-Z0-9-]*";
-            var regexUserID = @"User ID=[a-zA-Z0-9-_]*";
-            var regexPassword = @"Password=[a-zA-Z0-9-!@#$%^&*()_]*";
+        msg = Regex.Replace(msg, regexSQLConnectionString, cnstrReplaced);
+        msg = Regex.Replace(msg, regexInitialCatalog, catReplace);
+        msg = Regex.Replace(msg, regexUserID, userIDReplace);
+        msg = Regex.Replace(msg, regexPassword, passwordReplace);
+        return msg;
+    }
 
-            var cnstrReplaced = "[redacted-server]";
-            var catReplace = "Intiial Catalog=[redacted-db]";
-            var userIDReplace = "User ID=[redacted-user]";
-            var passwordReplace = "Password=[redacted-password]";
-
-            msg = Regex.Replace(msg, regexSQLConnectionString, cnstrReplaced);
-            msg = Regex.Replace(msg, regexInitialCatalog, catReplace);
-            msg = Regex.Replace(msg, regexUserID, userIDReplace);
-            msg = Regex.Replace(msg, regexPassword, passwordReplace);
-            return msg;
-        }
-
-        private static string SanitizeSASToken(string msg)
-        {
-            var regexSASToken = @"sig=[a-zA-Z0-9%]*";
-            var sasReplaced = "[sastoken]";
-            msg = Regex.Replace(msg, regexSASToken, sasReplaced);
-            return msg;
-        }
+    private static string SanitizeSASToken(string msg)
+    {
+        var regexSASToken = @"sig=[a-zA-Z0-9%]*";
+        var sasReplaced = "[sastoken]";
+        msg = Regex.Replace(msg, regexSASToken, sasReplaced);
+        return msg;
     }
 }
--- resources/Final/MVCProtectingSecrets/Program.cs
+++ resources/Final/MVCProtectingSecrets/Program.cs
@@ -1,4 +1,3 @@
-using Azure.Core;
 using Azure.Identity;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Identity;
@@ -6,79 +5,85 @@ using Microsoft.EntityFrameworkCore;
 using MVCProtectingSecrets.Data;
 using MVCProtectingSecrets.Initializers;
 
-namespace MVCProtectingSecrets;
-public class Program
+namespace MVCProtectingSecrets
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+        public static void Main(string[] args)
         {
-            var settings = config.Build();
+            var builder = WebApplication.CreateBuilder(args);
 
-            ////by default, use the default credential:
-            TokenCredential credential = new DefaultAzureCredential();
-            var env = settings["Application:Environment"];
+            /********************************************************************************************************
+            *  * Add Azure App Configuration
+            *  * use the connection string to connect to Azure App Configuration
+            *  * requires all app service and identity information to be authorized on app config [app data reader role, etc]
+            *  * for key vault => requires the user, app config, and app service to be authorized on the key vault
+            ********************************************************************************************************/
+            var appConfigConnection = builder.Configuration.GetConnectionString("AzureAppConfigConnection");
 
-            //without keyvault:
-            if (env == null || !env.Trim().Equals("develop", StringComparison.OrdinalIgnoreCase))
+            //WITHOUT KEY VAULT:
+            //builder.Configuration.AddAzureAppConfiguration(appConfigConnection);
+
+            //WITH KEY VAULT:
+            builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
             {
-                //if at azure, use ManagedIdentityCredential and AppConfiguration URI endpoint
-                credential = new ManagedIdentityCredential();
-
                 config.AddAzureAppConfiguration(options =>
-                    options.Connect(new Uri(settings["AzureAppConfigConnection"]), credential)
-                        .ConfigureKeyVault(kv => { kv.SetCredential(credential); }));
+                {
+                    options.Connect(appConfigConnection);
+                    options.ConfigureKeyVault(options =>
+                    {
+                        options.SetCredential(new DefaultAzureCredential());
+                    });
+                });
+            });
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            //optional, turn on to auto-migrate on startup [Warning: prevents any possibility of migration rollback]
+            //var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString).Options;
+            //using (var context = new ApplicationDbContext(contextOptions))
+            //{
+            //    context.Database.Migrate();
+            //}
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddApplicationInsightsTelemetry();
+            builder.Services.AddSingleton<ITelemetryInitializer, LogSanitizerInsightsInitializer>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
             }
             else
             {
-                //from local app, use connection string, not the URI
-                config.AddAzureAppConfiguration(options =>
-                        options.Connect(settings["AzureAppConfigConnection"])
-                                .ConfigureKeyVault(kv => { kv.SetCredential(credential); }));
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-        });
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
+            app.UseRouting();
 
-        builder.Services.AddApplicationInsightsTelemetry();
-        builder.Services.AddSingleton<ITelemetryInitializer, LogSanitizerInsightsInitializer>();
+            app.UseAuthorization();
 
-        var app = builder.Build();
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
         }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();
-
-        app.Run();
     }
 }
